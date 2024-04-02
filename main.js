934ede505fd85f5d903c3ce6b8c074bdb37264ec
Victor Samuel <victoress18@gmail.com>
2024-04-01 21:38:38 -0400

    Desordenar palabra, autotab, boton de reset y random, funcion de marcado de intentos y letras fallidas

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const scrambledWord = document.querySelector(".random-word");
 const wordContainer = document.getElementById("word");
 const randomButton = document.querySelector(".random");
+const resetButton = document.querySelector(".reset");
+const triesText = document.querySelector(".tries-text");
+const mistakes = document.querySelector(".letters-tried");
+const dots = document.querySelectorAll(".dot");
+const dotsArray = Array.from(dots);
 
-const words = ["flower", "table", "yellow", "common", "river", "fruit", "blue"];
+const words = ["raton", "silla", "fruta", "azul", "gato", "flor", "medico"];
 
+let i = 0;
 let tries = 0;
 let wordSorted = "";
 let word = words[Math.floor(Math.random() * words.length)];
@@ -27,13 +33,16 @@ const letterList = (word) => {
         let newLetter = document.createElement("input");
         newLetter.classList.add("letter");
         newLetter.setAttribute("type", "text");
+        newLetter.setAttribute("disabled", "");
+        newLetter.setAttribute("size", "1");
+        newLetter.setAttribute("maxlength", "1");
         wordContainer.appendChild(newLetter);
     }
 };
 
-const autoTab = () => {};
-
 letterList(word);
+const letterListElement = wordContainer.querySelectorAll(".letter");
+const letterArray = Array.from(letterListElement);
 
 scrambledWord.innerHTML = shuffle(word);
 
@@ -42,4 +51,35 @@ randomButton.addEventListener("click", () => {
     scrambledWord.innerHTML = shuffle(word);
     letterList(word);
     const firstLetter = document.getElementById("first").focus();
+});
+
+resetButton.addEventListener("click", () => {
+    tries = 0;
+    mistakes.innerHTML = "";
+    dots.forEach((dot) => {
+        dot.classList.remove("tried");
+    });
+    letterListElement.forEach((letter) => {
+        letter.setAttribute("disabled", "");
+        letter.value = "";
+    });
+    letterArray[0].removeAttribute("disabled");
+    letterArray[0].focus();
+    return tries;
+});
+
+addEventListener("keyup", () => {
+    if (letterArray[i].value.length === 1 && letterArray[i].value === word[i]) {
+        letterArray[i].setAttribute("disabled", "");
+        letterArray[i + 1].removeAttribute("disabled");
+        letterArray[i + 1].focus();
+        return i++;
+    } else if (letterArray[i].value !== word[i]) {
+        dotsArray[tries].classList.add("tried");
+        tries++;
+        triesText.innerText = `Tries(${tries}/5): `;
+        mistakes.innerHTML += letterArray[i].value + ",";
+        letterArray[i].value = "";
+        return tries;
+    }
 });
